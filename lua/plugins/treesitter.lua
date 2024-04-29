d12a12c41e1e4f45691cccd840fad5db29060808
Alexandre Sparton <sparton.alexandre@gmail.com>
2024-04-29 16:32:20 +0200

    feat(treesitter): ensure python + markdown installed

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     local configs = require("nvim-treesitter.configs")
     configs.setup({
-      ensure_installed = { "lua", "vim", "vimdoc", "query", "typescript", "javascript", "html" },
+      ensure_installed = { "lua", "vim", "vimdoc", "query", "typescript", "javascript", "html", "python", "markdown" },
       highlight = { enable = true },
       indent = { enable = true },
     })
