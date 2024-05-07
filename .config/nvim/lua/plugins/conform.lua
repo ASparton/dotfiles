d03f28ce07dcae835d137aa6b81c5793cc493325
Alexandre Sparton <sparton.alexandre@gmail.com>
2024-05-07 11:47:47 +0200

    feat(nvim): formatter added

---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    'stevearc/conform.nvim',
+    name = "conform.nvim",
+    dependencies = { "mason.nvim" },
+    config = function()
+      require("conform").setup({
+        formatters_by_ft = {
+          lua = { "stylua" },
+          -- Conform will run multiple formatters sequentially
+          python = { "ruff_format" },
+          -- Use a sub-list to run only the first available formatter
+          javascript = { { "prettierd", "prettier" } },
+        },
+      })
+      vim.keymap.set('n', '<space>f', function()
+        require("conform").format({ lsp_fallback = true })
+      end, {})
+    end
+  },
+  {
+    "zapling/mason-conform.nvim",
+    name = "mason-conform.nvim",
+    dependencies = { "conform.nvim" },
+    config = function()
+      require("mason-conform").setup()
+    end
+  }
+}
--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,43 +1,45 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-        },
-      })
-      require("mason-lspconfig").setup_handlers {
-        -- The first entry (without a key) will be the default handler
-        -- and will be called for each installed server that doesn't have
-        -- a dedicated handler.
-        function (server_name) -- default handler (optional)
-          require("lspconfig")[server_name].setup {}
-        end,
-     }
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      -- Keymaps
-      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
-      vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, {})
-      vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
-      vim.keymap.set({ 'n', 'v' }, '<space>!', vim.lsp.buf.code_action, {})
-      vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
-      vim.keymap.set('n', '<space>f', function()
-        vim.lsp.buf.format { async = true }
-      end, {})
-    end
-  }
+	{
+		"williamboman/mason.nvim",
+		name = "mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		name = "mason-lspconfig.nvim",
+		dependencies = { "mason.nvim" },
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+				},
+			})
+			require("mason-lspconfig").setup_handlers({
+				-- The first entry (without a key) will be the default handler
+				-- and will be called for each installed server that doesn't have
+				-- a dedicated handler.
+				function(server_name) -- default handler (optional)
+					require("lspconfig")[server_name].setup({})
+				end,
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		name = "nvim-lspconfig",
+		dependencies = { "mason-lspconfig.nvim" },
+		config = function()
+			-- Keymaps
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+			vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, {})
+			vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
+			vim.keymap.set({ "n", "v" }, "<space>!", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+		end,
+	},
 }
